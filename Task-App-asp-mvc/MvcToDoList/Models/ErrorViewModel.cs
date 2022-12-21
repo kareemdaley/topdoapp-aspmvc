46f3e751a782fcfeebf2a9953ac8f512f2c111c7
kareemdaley <90796170+kareemdaley@users.noreply.github.com>
2022-12-21 15:22:53 -0600

    Delete Task-App-asp-mvc directory

--- Task-App-asp-mvc/MvcToDoList/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcToDoList.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
